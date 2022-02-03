306f7198283880decbd884055cab402a2a0ecac4
salis010 <stephen.saliba@gmail.com>
2022-02-03 14:03:32 +0100

    fixup! Sets up Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import React, { FunctionComponent } from 'react'
 
 export const App: FunctionComponent = () => {
   return (
-    <h1>Hello world!</h1>
+    <h1 className='font-bold text-5xl text-red-300 ml-[-10px]'>Hello world!</h1>
   )
 }
