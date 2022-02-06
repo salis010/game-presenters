2b5132fe53f1c1713118b58d8b25cd435a78d430
salis010 <stephen.saliba@gmail.com>
2022-02-06 20:08:38 +0100

    Creates Presenters table

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
   ],
   content: [],
   theme: {
+    colors: {
+      secondary: {
+        50: 'rgb(255, 255, 255)',
+        100: 'rgb(200, 200, 200)',
+        200: 'rgb(75, 75, 75)'
+      },
+      presenter: {
+        50: 'rgb(178, 102, 255)',
+        100: 'rgb(153, 51, 255)',
+        200: 'rgb(76, 0, 153)'
+      },
+      table: {
+        100: 'rgb(150, 100, 100)'
+      },
+      schedule: {
+        100: 'rgb(41, 21, 155)',
+        200: 'rgb(18, 21, 153)'
+      }
+
+    },
     extend: {}
   },
   plugins: []
